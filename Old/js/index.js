6becbcd4c333b625d28d10302faaea661ae9464c
feretto <facundocapurata@gmail.com>
2023-05-24 18:27:14 +0200

    Add href to img

--- Old/js/index.js
+++
@@ -1,8 +0,0 @@
-function adjustFont() {
-  var factorZoom = window.devicePixelRatio;
-  var baseFont = 10;
-  var newFontSize = baseFont / factorZoom;
-  document.querySelector("#backgroundText").style.fontSize = newFontSize + "vw";
-}
-
-adjustFont();
